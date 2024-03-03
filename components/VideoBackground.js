11b6fe63127e509a05e6e52457179ebd5fdeb3f8
Ehan <techroot84@gmail.com>
2024-03-03 15:45:53 +0000

    first commit

---
+++ components/VideoBackground.js
@@ -0,0 +1,61 @@
+"use client"; 
+import React, { useEffect } from 'react';
+import VanillaTilt from 'vanilla-tilt';
+import styles from '../styles/styles.module.css'; 
+import ChildCard from './child';
+
+const VideoBackground = () => {
+  useEffect(() => {
+    const tiltContainer = document.querySelector('.tilt-container');
+    window.onload = function () {
+        document.getElementById('video-bg').play();
+    }
+
+    if (tiltContainer) {
+      VanillaTilt.init(tiltContainer, {
+        max: 10,
+        speed: 400,
+        glare: true,
+        'max-glare': 0.5,
+      });
+
+      return () => {
+        tiltContainer.vanillaTilt.destroy();
+      };
+    }
+  }, []);
+
+  return (
+    <div className={styles.videoContainer}>
+      <video autoPlay loop muted id="video-bg" className={styles.video}>
+        <source src="/media/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className={`${styles.content} tilt-container`}>
+      <div className={styles.profileImage}>
+    <img src="/pfp/pfp.png" alt="Profile" className={styles.profileImage} />
+
+  </div>
+  <div className={`${styles.name}`}>
+  <h3>DaDevGuy</h3>
+  </div>
+
+        <div className={styles.socialIcons}>
+  <a href="https://github.com/DaDevGuy" target="_blank" rel="noopener noreferrer">
+    <img src="/github.svg" alt="GitHub" className={`${styles.icon}`} data-name="GitHub" />
+  </a>
+  <a href="https://tobeadded.com/" target="_blank" rel="noopener noreferrer">
+    <img src="/bitcoin.svg" alt="BitCoin" className={`${styles.icon}`} data-name="YouTube" />
+  </a>
+  <a href="https://steamcommunity.com/profiles/76561199562233428/" target="_blank" rel="noopener noreferrer">
+    <img src="/steam.svg" alt="Steam" className={`${styles.icon}`} data-name="Steam" />
+  </a>
+
+</div>
+<ChildCard />
+      </div>
+    </div>
+  );
+};
+
+export default VideoBackground;
---
+++ components/child.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/child.module.css';
+
+const ChildCard = () => {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  const fetchQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.content);
+      })
+      .catch(error => console.error('Error fetching quote:', error));
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.profileSection}>
+        <img src="/pfp/pfp.png" alt="Profile" className={styles.profileImage} />
+        <h3 className={styles.username}>@gabepaki</h3>
+      </div>
+      <p>"{quote}"</p>
+      <a href="https://discord.com/users/1177943732402790501" target="_blank" rel="noopener noreferrer" className={styles.button}>Add on Discord</a>
+    </div>
+  );
+};
+
+export default ChildCard;
