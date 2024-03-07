d7088f71bdde42720a68b578ae8f3f5c3349a758
Ehan <techroot84@gmail.com>
2024-03-07 13:07:05 +0000

    Added Tailwind Component arrow

--- components/child.js
+++ components/child.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from '../styles/child.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import cx from 'classnames';
 
 const ChildCard = () => {
   const [quote, setQuote] = useState('');
@@ -21,7 +23,7 @@ const ChildCard = () => {
     <div className={styles.card}>
       <div className={styles.profileSection}>
         <img src="/pfp/pfp1.png" alt="Profile" className={styles.profileImage} />
-        <h3 className={styles.username}>@gabepaki</h3>
+        <h3 className={cx(styles.username, 'text-white', 'text-base')}>@gabepaki</h3>
         <div className={styles.badgeContainer}>
           <div className={styles.tooltip}>
             <img src="/dev.svg" alt="Steam" className={styles.badge} />
@@ -37,8 +39,21 @@ const ChildCard = () => {
           </div>
         </div>
       </div>
-      <p>"{quote}"</p>
-      <a href="https://discord.com/users/1177943732402790501" target="_blank" rel="noopener noreferrer" className={styles.button}>Add on Discord</a>
+      <p className="text-white">"{quote}"</p>
+      <a href="https://discord.com/users/1177943732402790501" target="_blank" rel="noopener noreferrer" className={styles.button} style={{ display: 'flex', alignItems: 'center' }}>
+        Add on Discord
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className={cx('w-6 h-6', 'text-blue-500')}
+          style={{ marginLeft: '5px' }}
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+        </svg>
+      </a>
     </div>
   );
 };
